ef260e1b2a2d142c3fe055dc2b6f9da9f52be4b9
Vicky K <devmrvicky@gmail.com>
2024-05-08 23:09:27 +0530

    add all remaing routes

--- server/routes/url.routes.js
+++ server/routes/url.routes.js
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import {
   createShortURL,
+  getAllShortUrlIds,
   redirectToMainURL,
+  deleteShortURLId,
+  getShortUrlAnalytics,
 } from "../controller/url.controller.js";
 
 const router = Router();
 
 router.route("/url").post(createShortURL);
+router.route("/short-url-ids").get(getAllShortUrlIds);
+router.route("/delete/:shortURLId").delete(deleteShortURLId);
 router.route("/:shortURLId").get(redirectToMainURL);
+router.route("/analytics/:shortURLId").get(getShortUrlAnalytics);
 
 export { router };
